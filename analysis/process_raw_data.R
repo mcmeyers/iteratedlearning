7b2806906dedf39244e3d62e92c826a8dbd6e23b
Dan Yurovsky <dyurovsky@gmail.com>
2018-07-31 10:51:31 -0500

    turk data processing

---
+++ analysis/process_raw_data.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(lubridate)
+library(jsonlite)
+
+files <- list.files("../raw_data/", ".json", full.names = T)
+
+read_data <- function(file) {
+  json_data <- read_json(file)
+  
+  data <- json_data$answers$data$data %>%
+    unlist() %>%
+    map(., ~ t(str_split(.x, ",", simplify = T))) %>%
+    bind_cols() %>%
+    t
+
+  data_df <- data[2:nrow(data),] %>%
+    as_data_frame()
+
+  names(data_df) <- str_trim(data[1,])
+  
+  data_df
+}
+
+map(files, read_data) %>%
+  bind_rows() %>%
+  write_csv("../data/anonymized_data.csv")
